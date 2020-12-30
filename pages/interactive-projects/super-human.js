42b00a8f6af914a440b9951773fc10335f8b4e9f
boweixiao <x.bowei@aol.com>
2020-12-30 07:13:59 +0100

    super human initialised

--- pages/interactive-projects/super-human.js
+++ pages/interactive-projects/super-human.js
@@ -1,8 +1,28 @@
+import styled from 'styled-components'
+import Image from 'next/image'
+import Layout from "../../components/Layout";
+
 const SuperHuman = () => {
     return (
-        <>
-        </>
+        <Layout>
+            <Section>
+                <Image src='/superHuman/titlePhoto.png' layout='fill' objectFit='cover' />
+            </Section>
+            <Section>
+                <Image src='/superHuman/comp2880.png' layout='fill' objectFit='cover' />
+            </Section>
+            <Section>
+
+            </Section>
+        </Layout>
     )
 }
+
+const Section = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  margin: -8px;
+`
 
 export default SuperHuman
