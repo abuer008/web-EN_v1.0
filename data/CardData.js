4a439b91bff899ab8fa30ab8fcef1161a85549f8
boweixiao <x.bowei@aol.com>
2021-05-11 03:24:02 +0200

    update text

--- data/CardData.js
+++ data/CardData.js
@@ -53,7 +53,7 @@ export const ImplementCardData = {
     interface: {
         title: 'Data-driven Interface',
         CNtitle: '数据驱动的原型',
-        text: '',
+        text: 'Provide data-driven, interactive, high-fidelity prototypes and front-end examples from a technical and design perspective',
         CNtext: '从技术与设计的角度出发，提供由数据驱动的可交互高保真原型与前端实例',
         imgSrc: '/data-visualisation/VisualDataMask.svg',
         link: '/interactive-projects/data-visualisation',
@@ -65,7 +65,7 @@ export const ImplementCardData = {
     implementation: {
         title: 'Frontend Implementation',
         CNtitle: '前端交互实现',
-        text: '',
+        text: 'Maintain sensitivity to front-end technology and consistency of design goals, providing a comprehensive user experience with a unique design process that guarantees the quality of the final product',
         CNtext: '保持对前端技术的敏感性和设计目标的一致性，提供全面的用户体验与独特化的设计流程，保障最终的产品质量',
         imgSrc: '/connecting/ConnectingMask.png',
         link: '/interactive-projects/connecting',
@@ -80,7 +80,7 @@ export const DesignMetricsData = {
     value: {
         title: 'Value User Experience',
         CNtitle: '评估与产品反馈',
-        text: '',
+        text: 'Use HEART framework, SUS (System Usability Scale) and KPI to collect user feedback systematically and continuously improve product interaction experience.',
         CNtext: "使用HEART框架，SUS(System Usability Scale)和KPI等，系统化的收集用户反馈与持续性的提高产品交互体验",
         imgSrc: '/DesignMetricsMask.png',
         link: '',
@@ -92,11 +92,11 @@ export const DesignMetricsData = {
     iteration: {
         title: 'Design Iteration',
         CNtitle: '设计迭代',
-        text: '',
+        text: 'Meet the needs of expanding users; build, expand and maintain unique design language and patterns that make the product stand out from the competitors.',
         CNtext: '满足不断扩大的用户需求；建立，扩展与维护独有的设计语言与模式，让产品从众多同类中脱颖而出',
         imgSrc: '/aal/SdpMask.svg',
         link: '/work/SDP',
-        cardName: 'Salaries Distribution Platform',
+        cardName: 'Bonus Distribution Platform',
         CNcardName: '税务代征平台',
         backgroundColor: '#333',
         shadowColor: 'rgba(51, 51, 51, 0.3)'
@@ -107,7 +107,7 @@ export const InteractiveProjectsData = {
     language: {
         title: 'Language',
         CNtitle: '语义化交互',
-        text: 'Focuses on the future and user needs to provide products with sustainable and irreplaceable competitive advantages',
+        text: 'Interaction design is the design of the language, and discovering more efficient ways to communicate between individuals and with the environment is a major trend in the future of interaction.',
         CNtext: '交互设计是语言的设计，发现个体之间和与环境之间更有效率的交流方式是未来交互方式的大趋势',
         imgSrc: '/revision/RevisionMask.svg',
         link: '/interactive-projects/revision',
@@ -119,7 +119,7 @@ export const InteractiveProjectsData = {
     performance: {
         title: 'Performance',
         CNtitle: '交互性能',
-        text: 'Focuses on the future and user needs to provide products with sustainable and irreplaceable competitive advantages',
+        text: 'Understanding technology determines the form of interaction and is an important factor that directly affects the final user experience.',
         CNtext: '对技术的理解决定了交互的形式，也是直接影响最终的用户体验的重要因素',
         imgSrc: '/data-visualisation/VisualDataMask.svg',
         link: '/interactive-projects/data-visualisation',
@@ -131,7 +131,7 @@ export const InteractiveProjectsData = {
     strategicThinking: {
         title: 'Strategic Thinking',
         CNtitle: '战略化思维',
-        text: 'Focuses on the future and user needs to provide products with sustainable and irreplaceable competitive advantages',
+        text: 'Analyze and understand the meaning and future of products in a macroscopic dimension, and give them great social value and potential.',
         CNtext: '以宏观的维度去分析与理解产品的意义与未来，赋予产品巨大的社会价值与潜力',
         imgSrc: '/superHuman/SuperHumanMask.svg',
         link: '/interactive-projects/super-human',
@@ -143,7 +143,7 @@ export const InteractiveProjectsData = {
     designThinking: {
         title: 'Design Thinking',
         CNtitle: '设计思维',
-        text: 'Focuses on the future and user needs to provide products with sustainable and irreplaceable competitive advantages',
+        text: 'Follow a rigorous design methodology and mindset to propose unique and systematic solutions.',
         CNtext: '跟随严格的设计方法论与思维模式，推导出独特的系统化解决方案',
         imgSrc: '/aal/AalMask.svg',
         link: '/interactive-projects/aal',
@@ -153,9 +153,9 @@ export const InteractiveProjectsData = {
         shadowColor: 'rgba(31, 159, 113, 0.3)'
     },
     conclusion: {
-        title: 'Comprehensive Process',
+        title: 'Design Epistemology',
         CNtitle: '三位一体的设计认识论',
-        text: 'Focuses on the future and user needs to provide products with sustainable and irreplaceable competitive advantages',
+        text: 'The conceptual idea, technology, and design, all three unified in the new design epistemology, is a new design process for the next generation.',
         CNtext: '概念化，技术与设计，三者结合的设计认识论，是次世代全新的设计流程。',
         imgSrc: '/connecting/ConnectingMask.png',
         link: '/interactive-projects/connecting',
